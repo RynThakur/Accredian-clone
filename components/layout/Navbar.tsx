f37f2ad40d7317ffb684e3b4367490466fa20efa
Aryan Thakur <rynthakur@gmail.com>
2026-04-22 00:03:18 +0530

    fix import issues in Navbar.tsx and Hero.tsx

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { Menu, X } from "lucide-react";
-import Button from "../ui/button";
+import Button from "../ui/Button";
 
 export default function Navbar(){
     const[isOpen, setIsOpen] = useState(false);
--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,5 +1,5 @@
 import { CheckCircle2 } from "lucide-react";
-import Button from "../ui/button";
+import Button from "../ui/Button";
 
 export default function Hero() {
   const benefits = [
